69a4e2bbb97a9f3974a23ea389441fbdaa4f9118
zalesiu <zalewmat31@gmail.com>
2024-03-04 15:26:11 +0100

    feat: create graphql schema

---
+++ schema.js
@@ -0,0 +1,27 @@
+import { gql } from "@apollo/server";
+
+export const typeDefs = gql`
+  type Game {
+    id: ID!
+    title: String!
+    platform: [String]!
+  }
+
+  type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+  }
+
+  type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+  }
+
+  type Query {
+    reviews: [Reviews]
+    games: [Game]
+    authors: [Author]
+  }
+`;
