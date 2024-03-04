1c7ae58cb543799ad74ed3180bd36bacc2ebcaef
zalesiu <zalewmat31@gmail.com>
2024-03-04 22:38:59 +0100

    feat: add resolvers object to index

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
+import db from "./db";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.games;
+    },
+    authors() {
+      return db.games;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-  // resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
