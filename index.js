6ff5f95f77caee724a8bc7e23bd25264da4e51a2
zalesiu <zalewmat31@gmail.com>
2024-03-05 13:51:28 +0100

    feat: create resolver functions

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
-import db from "./db";
+import { typeDefs } from "./schema.js";
+import db from "./db.js";
 
 const resolvers = {
   Query: {
     games() {
       return db.games;
     },
-    reviews() {
-      return db.games;
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
     },
     authors() {
-      return db.games;
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((author) => author.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((game) => game.id === parent.game_id);
     },
   },
 };
