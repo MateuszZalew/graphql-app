5e7e39d4ba37d5d86e0a75af890515dfb7cfbc50
zalesiu <zalewmat31@gmail.com>
2024-03-05 13:49:48 +0100

    feat: add data relation types in schema

--- schema.js
+++ schema.js
@@ -1,27 +1,32 @@
-import { gql } from "@apollo/server";
-
-export const typeDefs = gql`
+export const typeDefs = `#graphql
   type Game {
     id: ID!
     title: String!
-    platform: [String]!
+    platform: [String!]!
+    reviews: [Review!]
   }
 
   type Review {
     id: ID!
     rating: Int!
     content: String!
+    game: Game!
+    author: Author!
   }
 
   type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
   }
 
   type Query {
     reviews: [Review]
+    review(id: ID!): Review
     games: [Game]
+    game(id: ID!): Game
     authors: [Author]
+    author(id: ID!): Author 
   }
 `;
